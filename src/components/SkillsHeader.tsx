c7ec39f83f30c1c408ae8b11b2cf84db62a3e487
Your Name <you@example.com>
2025-07-26 12:46:48 +0900

    SkillsHeaderコンポーネントとスタイルを追加

---
+++ src/components/SkillsHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SkillsHeader.css';
+
+const SkillsHeader: React.FC = () => {
+  return (
+    <header className="skills-header">
+      <h1>Skills</h1>
+    </header>
+  );
+};
+
+export default SkillsHeader;
